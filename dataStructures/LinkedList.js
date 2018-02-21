22ca4406f5d7858cea9c63549b2070c0095d3a8e
Dinir Nertan <dinirnertan@gmail.com>
2018-02-22 00:25:00 +0900

    Linked List

---
+++ dataStructures/LinkedList.js
@@ -0,0 +1,86 @@
+function LinkedList() {
+  this.head = null;
+  this.tail = null;
+}
+function Node(value, next, prev) {
+  this.value = value;
+  this.next = next;
+  this.prev = prev;
+}
+
+LinkedList.prototype.addToHead = function(value) {
+  const newNode = new Node(value, this.head, null);
+  if(this.head)
+    this.head.prev = newNode;
+  else
+    this.tail = newNode;
+  this.head = newNode;
+};
+LinkedList.prototype.addToTail = function(value) {
+  const newNode = new Node(value, null, this.tail);
+  if(this.tail)
+    this.tail.next = newNode;
+  else
+    this.head = newNode;
+  this.tail = newNode;
+};
+
+LinkedList.prototype.removeHead = function() {
+  if(!this.head) return null;
+  const val = this.head.value;
+  this.head = this.head.next;
+  if(this.head)
+    this.head.prev = null;
+  else
+    this.tail = null;
+  return val;
+};
+LinkedList.prototype.removeTail = function() {
+  if(!this.tail) return null;
+  const val = this.tail.value;
+  this.tail = this.tail.prev;
+  if(this.tail)
+    this.tail.next = null;
+  else
+    this.head = null;
+  return val;
+};
+
+LinkedList.prototype.search = function(searchValue) {
+  let currentNode = this.head;
+  while (currentNode) {
+    if(currentNode.value === searchValue) return currentNode.value;
+    currentNode = currentNode.next;
+  }
+  return null;
+};
+LinkedList.prototype.indexOf = function(searchValue) {
+  if(!this.head) return null;
+  const indexes = [];
+  let currentIndex = 0;
+  let currentNode = this.head;
+  while(currentNode) {
+    if(currentNode.value === searchValue) indexes.push(currentIndex);
+    currentNode = currentNode.next;
+    currentIndex++;
+  }
+  switch(indexes.length) {
+    case 1:
+      return indexes[0];
+    case 0:
+      return null;
+    default:
+      return indexes;
+  }
+};
+
+let ll = new LinkedList();
+
+ll.addToTail(3);
+ll.addToTail(5);
+ll.addToTail(3);
+ll.addToTail(8);
+ll.addToTail(7);
+ll.addToTail(4);
+
+console.log(ll.indexOf(3));
---
+++ dataStructures/readme.md
@@ -0,0 +1,19 @@
+# Associated Course
+[Learning Data Structures in JavaScript from Scratch][Course Link]
+
+Write more efficient & performant code by learning data structures. Be well prepared for technical interview questions.
+
+4.7 (874 ratings)  
+8,050 students enrolled  
+Created by Eric Traub  
+Last updated 1/2018
+
+# Index
+
+- [LinkedList.js][1]  Section 2. Linked List
+
+
+
+[Course Link]: https://www.udemy.com/learning-data-structures-in-javascript-from-scratch/ "Learning Data Structures in JavaScript from Scratch"
+
+[1]: LinkedList.js
