0dbdc84819fbe796162033ee223698f8176bbbd9
Dinir Nertan <DinirNertan@gmail.com>
2018-03-26 23:46:57 +0900

    Send messages to only people in same room
    modify the message sending logic to send correct user name

--- nodejs/node-chat-app/public/js/chat.js
+++ nodejs/node-chat-app/public/js/chat.js
@@ -79,7 +79,6 @@ $('#message-form').on('submit', function(e) {
   var messageTextbox = jQuery('[name=message]');
 
   socket.emit('createMessage', {
-    from: 'User',
     text: messageTextbox.val()
   }, function() {
     messageTextbox.val('');
--- nodejs/node-chat-app/server/server.js
+++ nodejs/node-chat-app/server/server.js
@@ -68,18 +68,23 @@ io.on('connection', socket => {
   });
 
   socket.on('createMessage', (message, callback) => {
-    console.log(`New message from ${message.from}: `);
-    console.log(message.text);
+    const user = users.getUser(socket.id);
 
-    // `io.emit` emits event to every connected user
-    io.emit('newMessage', generateMessage(message.from, message.text));
+    if(user && isRealString(message.text)) {
+      io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+    }
 
     callback();
   });
 
   socket.on('createLocationMessage', (coords) => {
-    console.log('New location message:', coords);
-    io.emit('newLocationMessage', generateLocationMessage('Server', coords.latitude, coords.longitude));
+    const user = users.getUser(socket.id);
+
+    if(user) {
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(
+        user.name, coords.latitude, coords.longitude
+      ));
+    }
   });
 });
 
